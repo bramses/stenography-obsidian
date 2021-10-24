39fed91fefb28792e49336698f7e4beb5efadb0c
Bram Adams <3282661+bramses@users.noreply.github.com>
2021-10-24 15:05:29 -0400

    main

--- main.ts
+++ main.ts
@@ -18,7 +18,7 @@ export default class StenographyPlugin extends Plugin {
 		this.addRibbonIcon('code-glyph', 'Stenography', async () => {
 			await this.stenographyWorkflow();
 		});
-		
+
 		this.addCommand({
 			id: 'run-stenograpy',
 			name: 'Run Stenography',
@@ -27,69 +27,76 @@ export default class StenographyPlugin extends Plugin {
 			}
 		});
 
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addSettingTab(new StenographyPluginTab(this.app, this));
 
 
 	}
 
 	async stenographyWorkflow() {
-		const editor = this.app.workspace.getActiveViewOfType(MarkdownView)?.editor; 
-		const selectedText = editor.getSelection(); // Get selected text
-		if (selectedText && selectedText.length > 0) {
-			const res = await this.fetchStenography(selectedText)
-			editor.replaceSelection(`${res.res}\n\n\`\`\`${res.language}\n${selectedText}\n\`\`\`\n\n`)
+		try {
+			if (this.app.workspace.getActiveViewOfType(MarkdownView)) {
+				const editor = this.app.workspace.getActiveViewOfType(MarkdownView).editor;
+				const selectedText = editor.getSelection(); // Get selected text
+				if (selectedText && selectedText.length > 0) {
+					const res = await this.fetchStenography(selectedText)
+					editor.replaceSelection(`${res.res}\n\n\`\`\`${res.language}\n${selectedText}\n\`\`\`\n\n`)
+				}
+			} else {
+				throw new Error('MarkdownView is null');
+			}
+		} catch (err) {
+			new Notice(err.message);
 		}
 	}
 
-	async fetchStenography(code: string): Promise<any> {
-	 const statusHTML = this.addStatusBarItem()
-	 statusHTML.setText('Loading from Stenography...');
-	 return await fetch('https://stenography-worker.stenography.workers.dev',
-      {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json;charset=UTF-8'},
-        body: JSON.stringify({
-          code: code,
-          api_key: this.settings.apiKey,
-          audience: 'pm',
-          populate: false,
-          stackoverflow: false
-        })
-      }).then(resp => 
-      {
-        return resp.json()
-      }).then((data:any) => {
-		var markdown = data.pm
-		const language = data.metadata.language || ''
-		if (markdown && Object.keys(markdown).length === 0 && Object.getPrototypeOf(markdown) === Object.prototype) {
-			return {res: `Stenography response empty!`, language: ''}
+	async fetchStenography(code: string): Promise < any > {
+			const statusHTML = this.addStatusBarItem()
+		statusHTML.setText('Loading from Stenography...');
+			try {
+				const res = await fetch('https://stenography-worker.stenography.workers.dev',
+					{
+						method: 'POST',
+						headers: { 'Content-Type': 'application/json;charset=UTF-8' },
+						body: JSON.stringify({
+							code: code,
+							api_key: this.settings.apiKey,
+							audience: 'pm',
+							populate: false,
+							stackoverflow: false
+						})
+					})
+			const data = await res.json();
+				var markdown = data.pm
+			const language = data.metadata.language || ''
+			if(markdown && Object.keys(markdown).length === 0 && Object.getPrototypeOf(markdown) === Object.prototype) {
+			return { res: `Stenography response empty!`, language: '' }
 		}
 		return { res: markdown, language: language }
-	  }).catch(err => {
-		  console.log(err.message)
-		  console.log(err.message.includes('Failed to fetch'))
-		  if (err.message.includes('Failed to fetch')) { return {res: `Error loading from Stenography! API key error, did you set it in settings?`, language: ''} }
-		  else return {res: `Error loading from Stenography! Error: ${err}`, language: ''}
-		})
-	  .finally(() => {
+	} catch(err) {
+		console.log(err.message)
+		console.log(err.message.includes('Failed to fetch'))
+		if (err.message.includes('Failed to fetch')) { return { res: `Error loading from Stenography! API key error, did you set it in settings?`, language: '' } }
+		else return { res: `Error loading from Stenography! Error: ${err}`, language: '' }
+	} finally {
 		statusHTML.remove();
-	  })
 	}
 
-	onunload() {
-		console.log('unloading stenography plugin');
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+onunload() {
+	console.log('unloading stenography plugin');
 }
 
-class SampleSettingTab extends PluginSettingTab {
+	async loadSettings() {
+	this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+}
+
+	async saveSettings() {
+	await this.saveData(this.settings);
+}
+}
+
+class StenographyPluginTab extends PluginSettingTab {
 	plugin: StenographyPlugin;
 
 	constructor(app: App, plugin: StenographyPlugin) {
@@ -98,11 +105,11 @@ class SampleSettingTab extends PluginSettingTab {
 	}
 
 	display(): void {
-		let {containerEl} = this;
+		let { containerEl } = this;
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for Stenography.'});
+		containerEl.createEl('h2', { text: 'Settings for Stenography.' });
 
 		// for api key
 		new Setting(containerEl)
@@ -115,6 +122,6 @@ class SampleSettingTab extends PluginSettingTab {
 					this.plugin.settings.apiKey = value;
 					await this.plugin.saveSettings();
 					new Notice('Saved API key!');
-				}));		
+				}));
 	}
 }
